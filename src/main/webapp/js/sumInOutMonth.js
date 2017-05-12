3bb6192267bd497366a4220316dcaf933cffe1a9
ye-huanhuan <13244237736@163.com>
2017-05-12 20:30:14 +0800

    完成请求json数据

--- src/main/webapp/js/sumInOutMonth.js
+++ src/main/webapp/js/sumInOutMonth.js
@@ -2,7 +2,13 @@
  * 进销项总和对比
  */
 	//运用构造函数式
-     var chart = new Highcharts.Chart('container_top', {
+
+var data_difference;
+var data_input;
+var data_output;
+var chart;
+$(function () {
+     chart = new Highcharts.Chart('container_top', {
     	
         title: {
             text: '季度分析表',
@@ -53,11 +59,118 @@
              {
 			type: 'spline',
             name: '进项金额',
-            data: [16, 15, 16, 15, 11, 13, 17, 13, 13, 16, 14, 12]
+             
         }, {
         	type: 'spline',
         	name: '销项金额',
-            data: [13, 16, 18, 11, 13, 17, 19, 13, 16, 18, 15, 17]
+             
         }
         ]
    });
+     $("#year-2016").attr("checked","checked");
+     $.ajax({
+    	    async: true,
+    	    data: {year:$("#year-2016").val()},
+    	    type: "post",        //type：(string)请求方式，POST或GET
+    	    dataType: "json",    //dataType：(string)预期返回的数据类型。xml,html,json,text等
+//    	    url: "jsondate.json",  //url：(string)发送请求的地址，可以是服务器页面也可以是WebService动作。
+    	    url: "test_testJson.action",
+    	    success: function (msg) {
+    	    	
+    	    	alert(msg);
+    	        var obj = eval(msg);
+    	        data_difference = obj["data_difference"];
+    	        data_input = obj["data_input"];
+    	        data_output = obj["data_output"];
+//    	        newdate = [obj["1"],obj["2"],obj["3"],obj["4"],obj["5"],obj["6"],obj["7"],obj["8"],obj["9"],obj["10"],obj["11"],obj["12"]];
+    	        alert(data_difference);
+    	        chart.series[0].setData(data_difference);
+    	        chart.series[1].setData(data_input);
+    	        chart.series[2].setData(data_output);
+    	    }
+    	});
+});
+//发送异步请求2016年数据
+$(function () {
+    $("#year-2016").click(function () {
+    	
+        $.ajax({
+            async: true,
+            data: {year:$("#year-2016").val()},
+            type: "post",        //type：(string)请求方式，POST或GET
+            dataType: "json",    //dataType：(string)预期返回的数据类型。xml,html,json,text等
+//            url: "jsondate.json",  //url：(string)发送请求的地址，可以是服务器页面也可以是WebService动作。
+            url: "test_testJson.action",
+            success: function (msg) {
+            	
+            	alert(msg);
+                var obj = eval(msg);
+                data_difference = obj["data_difference"];
+                data_input = obj["data_input"];
+                data_output = obj["data_output"];
+//                newdate = [obj["1"],obj["2"],obj["3"],obj["4"],obj["5"],obj["6"],obj["7"],obj["8"],obj["9"],obj["10"],obj["11"],obj["12"]];
+                alert(data_difference);
+                chart.series[0].setData(data_difference);
+                chart.series[1].setData(data_input);
+                chart.series[2].setData(data_output);
+            }
+        });
+    });
+});
+//发送异步请求2017年数据
+$(function () {
+    $("#year-2017").click(function () {
+    	
+        $.ajax({
+            async: true,
+            data: {year:$("#year-2017").val()},
+            type: "post",        //type：(string)请求方式，POST或GET
+            dataType: "json",    //dataType：(string)预期返回的数据类型。xml,html,json,text等
+//            url: "jsondate.json",  //url：(string)发送请求的地址，可以是服务器页面也可以是WebService动作。
+            url: "test_testJson.action",
+            success: function (msg) {
+            	
+            	alert(msg);
+                var obj = eval(msg);
+                data_difference = obj["data_difference"];
+                data_input = obj["data_input"];
+                data_output = obj["data_output"];
+//                newdate = [obj["1"],obj["2"],obj["3"],obj["4"],obj["5"],obj["6"],obj["7"],obj["8"],obj["9"],obj["10"],obj["11"],obj["12"]];
+                alert(data_difference);
+                chart.series[0].setData(data_difference);
+                chart.series[1].setData(data_input);
+                chart.series[2].setData(data_output);
+            }
+        });
+    });
+});
+//发送异步请求2015年数据
+$(function () {
+    $("#year-2015").click(function () {
+    	
+        $.ajax({
+            async: true,
+            data: {year:$("#year-2015").val()},
+            type: "post",        //type：(string)请求方式，POST或GET
+            dataType: "json",    //dataType：(string)预期返回的数据类型。xml,html,json,text等
+//            url: "jsondate.json",  //url：(string)发送请求的地址，可以是服务器页面也可以是WebService动作。
+            url: "test_testJson.action",
+            success: function (msg) {
+            	
+            	alert(msg);
+                var obj = eval(msg);
+                data_difference = obj["data_difference"];
+                data_input = obj["data_input"];
+                data_output = obj["data_output"];
+//                newdate = [obj["1"],obj["2"],obj["3"],obj["4"],obj["5"],obj["6"],obj["7"],obj["8"],obj["9"],obj["10"],obj["11"],obj["12"]];
+                alert(data_difference);
+                chart.series[0].setData(data_difference);
+                chart.series[1].setData(data_input);
+                chart.series[2].setData(data_output);
+            }
+        });
+    });
+});
+
+//初始化highchart
+
